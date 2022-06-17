644cdb8e1c64e7c0efa91b4878a96097f2185491
panzhan <phy422915432@163.com>
2022-06-17 23:36:22 +0800

    feat: test git

--- src/Basic/NativeJs/Calculate.js
+++ src/Basic/NativeJs/Calculate.js
@@ -6,83 +6,64 @@
 
 // 小数点位数获取
 const getDecimalLength = ({ x, y } = {}) => {
-    let lx, ly, lmax
-    try {
-        lx = x.toString().split('.')[1].length
-    } catch {
-        lx = 0
-    }
-    try {
-        ly = y.toString().split('.')[1].length
-    } catch {
-        ly = 0
-    }
-    lmax = Math.max(lx, ly)
-    return {
-        lx, ly, lmax
-    }
+  let lx, ly, lmax
+  try {
+    lx = x.toString().split('.')[1].length
+  } catch {
+    lx = 0
+  }
+  try {
+    ly = y.toString().split('.')[1].length
+  } catch {
+    ly = 0
+  }
+  lmax = Math.max(lx, ly)
+  return { lx, ly, lmax }
 }
 // 加
 const addition = ({ x, y } = {}) => {
-    const {
-        lx = 0,
-        ly = 0,
-        lmax = 0
-    } = getDecimalLength({ x, y })
-    return (x * Math.pow(10, lx) + y * Math.pow(10, ly)) / Math.pow(10, lmax)
+  const { lmax } = getDecimalLength({ x, y } = {})
+  return (x * Math.pow(10, lmax) + y * Math.pow(10, lmax)) / Math.pow(10, lmax)
 }
 // 减
 const subtraction = ({ x, y } = {}) => {
-    const {
-        lmax = 0
-    } = getDecimalLength({ x, y })
-    return (x * Math.pow(10, lmax) - y * Math.pow(10, lmax)) / Math.pow(10, lmax)
+  const { lmax } = getDecimalLength({ x, y })
+  return (x * Math.pow(10, lmax) - y * Math.pow(10, lmax)) / Math.pow(10, lmax)
 }
 // 乘
-const multiplication = ({ x, y }) => {
-    const {
-        lx = 0,
-        ly = 0
-    } = getDecimalLength({ x, y })
-    return (x * Math.pow(10, lx)) * (y * Math.pow(10, ly)) / Math.pow(10, lx + ly)
+const multiplication = ({ x, y } = {}) => {
+  const { lx, ly } = getDecimalLength({ x, y } = {})
+  return (x * Math.pow(10, lx)) * (y * Math.pow(10, ly)) / Math.pow(10, lx + ly)
 }
 // 除
 const division = ({ x, y } = {}) => {
-    const {
-        lmax = 0
-    } = getDecimalLength({ x, y })
-    return (x * Math.pow(10, lmax)) / (y * Math.pow(10, lmax))
+  const { lmax } = getDecimalLength({ x, y })
+  return (x * Math.pow(10, lmax)) / (y * Math.pow(10, lmax))
 }
 // 保留n位小数
 const intercept = ({ resources, digits } = {}) => {
-    const {
-        lx = 0
-    } = getDecimalLength({ x: resources })
-    if (lx == digits) {
-        return resources
+  const { lx } = getDecimalLength({ x: resources })
+  if (lx == digits) {
+    return resources
+  }
+  if (lx < digits) {
+    for (let i = 0; i < digits - lx; i++) {
+      resources += '0'
     }
-    if (lx < digits) {
-        for (let i = 0; i < digits - lx; i++) {
-            resources += '0'
-        }
-        return resources
-    }
-    if (lx > digits) {
-        let result = resources + ''
-        return result.substring(0, result.length - (lx - digits))
-    }
+    return resources
+  }
+  if (lx > digits) {
+    let result = resources + ''
+    return result.substring(0, result.length - (lx - digits))
+  }
 }
-// let testCalc = [100, 5.55]
-// let divisionRes = division({ x: testCalc[0], y: testCalc[1] })
-// let interceptRes = intercept({ resources: divisionRes, digits: 3 })
-// console.log(interceptRes)
 
 module.exports = {
-    getDecimalLength, // 小数点位数获取
-    addition, // 加
-    subtraction, // 减
-    multiplication, // 乘
-    division, // 除
-    intercept // 保留n位小数
+  getDecimalLength, // 小数点位数获取
+  addition, // 加
+  subtraction, // 减
+  multiplication, // 乘
+  division, // 除
+  intercept // 保留n位小数
 }
 
